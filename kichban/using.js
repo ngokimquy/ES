c05ce21203561f1a4d8ac8facd794fa52f64af2e
ngokimquy <31992923+ngokimquy@users.noreply.github.com>
2018-05-10 01:18:10 +0700

    test
    none

---
+++ kichban/using.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+
+    var id = "12345";
+var content="clickhere";
+
+
+var text ="<button " + 'id=123456' + ">" +"btn1" + "</button>"; 
+    $("#btnthem").click(function(){
+        $(".btngroup").append(text);
+
+    })
+
+
+
+
+
+   $("#btnset").click(function(){
+
+
+    openNav();
+
+   }) 
+
+
+
+
+
+
+
+
+    })
