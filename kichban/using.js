3fdf69616f368c173520306ce01e2b539a56dd89
ngokimquy <31992923+ngokimquy@users.noreply.github.com>
2018-05-10 13:16:57 +0700

    aklguioag
    anhdung_themkashg

--- kichban/using.js
+++ kichban/using.js
@@ -1,14 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
 
     var id = "12345";
-var content="clickhere";
+    var content = "clickhere";
 
 
-var text ="<button " + 'id=123456' + ">" +"btn1" + "</button>"; 
-    $("#btnthem").click(function(){
-        $(".btngroup").append(text);
+    var text1 = "<button " + 'id = ' + id + ">" + content + "</button>";
+    var text2 = "<button " + 'id=123456' + ">" + "btn2" + "</button>";
+    var text3 = "<button " + 'id=123456' + ">" + "btn1" + "</button>";
+    var text4 = "<button " + 'id=123456' + ">" + "btn1" + "</button>";
+    var text5 = "<button " + 'id=123456' + ">" + "btn1" + "</button>";
+    var text6 = "<button " + 'id=123456' + ">" + "btn1" + "</button>";
+    var text7 = "<button " + 'id=123456' + ">" + "btn1" + "</button>";
+    var text8 = "<button " + 'id=123456' + ">" + "btn1" + "</button>";
+
+
+
+    var dem = 1;
+
+    $("#btnthem").click(function() {
+
+
+        $(".btngroup").append(text1);
+
+
+
+
+
+    })
+
+
+
+
+
+    $("#btnset").click(function() {
+
+
+        openNav();
 
     })
 
@@ -16,18 +45,7 @@ var text ="<button " + 'id=123456' + ">" +"btn1" + "</button>";
 
 
 
-   $("#btnset").click(function(){
-
-
-    openNav();
-
-   }) 
 
 
 
-
-
-
-
-
-    })
+})
